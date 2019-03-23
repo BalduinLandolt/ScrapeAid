35627a10052c818885d6f7a9569bd4c1fa1368d8
BalduinLandolt <balduin.landolt@unibas.ch>
2019-03-23 19:50:18 +0100

    Add: call_url() method

--- bin/scraper.py
+++ bin/scraper.py
@@ -1,3 +1,9 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
 class Scraper:
     """
     Class 'Scraper' that does all the scraping.
@@ -9,6 +15,18 @@ class Scraper:
         """
 
         print("Initialized an instance of Scraper")
+        return
+
+    def call_url(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+
+        print("calling: {}".format(url))
+
+        return 0
 
     def run(self):
         """
@@ -17,10 +35,14 @@ class Scraper:
         :return: Exit code: 0 if successful; -1 if not.
         """
 
-        print("Running Scraper...")
+        print("Running Scraper...\n")
 
         exit_code = 0
 
-        #TODO do stuff here
+# TODO do stuff here
+
+# TODO Test URL. Should be solved by dynamic input
+        url = "http://www.nietzschesource.org/"
+        self.call_url(url)
 
         return exit_code
