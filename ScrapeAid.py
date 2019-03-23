17d81d0c83a87fa89ee67277c13ef615501594a3
BalduinLandolt <balduin.landolt@unibas.ch>
2019-03-23 19:20:21 +0100

    Add: Launcher structure

--- ScrapeAid.py
+++ ScrapeAid.py
@@ -4,11 +4,15 @@ Launcher for the ScrapeAid application.
 Run as __main__ to automatically launch the program.
 """
 
+import bin.scraper as sc
+
 print("Starting launcher")
 
 
 def main():
     print("Launcher's main() method called.")
+    scraper = sc.Scraper()
+    print("Got Scraper: {}".format(scraper))
     return
 
 
---
+++ bin/scraper.py
@@ -0,0 +1,11 @@
+class Scraper:
+    """
+    Class 'Scraper' that does all the scraping.
+    """
+
+    def __init__(self):
+        """
+        Constructor for the 'Scraper' Class.
+        """
+
+        print("Initialized an instance of Scraper")
