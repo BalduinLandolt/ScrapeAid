e2a0a6b11e1c36ea7342aaa21d4d7fa82f12b88e
BalduinLandolt <balduin.landolt@unibas.ch>
2019-03-23 19:08:18 +0100

    Add: Main Project Structure set up

---
+++ ScrapeAid.py
@@ -0,0 +1,16 @@
+"""
+Launcher for the ScrapeAid application.
+
+Run as __main__ to automatically launch the program.
+"""
+
+print("Starting launcher")
+
+
+def main():
+    print("Launcher's main() method called.")
+    return
+
+
+if __name__ == '__main__':
+    main()
